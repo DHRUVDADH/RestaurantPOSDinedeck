a3502070381fe6aa1ecd399ea836af07bb71052e
DHRUVDADH <dhruvdadhania9@gmail.com>
2025-02-14 12:55:54 +0530

    LoginResponsive

--- pos-frontend/src/pages/Auth.jsx
+++ pos-frontend/src/pages/Auth.jsx
@@ -10,19 +10,19 @@ const Auth = () => {
       <div className="h-fit w-fit text-4xl text-custom-pink mb-24">DineDeck</div>
 
 
-      <form className="h-fit w-fit bg-custom-bg-2 rounded-2xl px-12 py-16 flex flex-col items-center justify-center">
+      <form className="h-fit sm:w-fit w-full bg-custom-bg-2 rounded-2xl px-12 py-16 flex flex-col items-center justify-center">
 
         <div className="text-3xl">Login!</div>
-        <div className="text-sm mt-6">Please enter your credentials below to continue</div>
+        <div className="text-sm mt-6 hidden sm:block">Please enter your credentials below to continue</div>
 
 
-        <div className="w-[450px] mt-12 flex flex-col items-start">
+        <div className="sm:w-[450px] w-[280px] mt-12 flex flex-col items-start">
           <label className="text-[11px] ml-1" htmlFor="username">Username</label>
           <input className="bg-custom-input-bg rounded-md w-full px-2 py-2 mt-1" type="text" id="username" />
         </div>
 
 
-        <div className="w-[450px] mt-7 flex flex-col items-start">
+        <div className="sm:w-[450px] w-[280px] mt-7 flex flex-col items-start">
           <label className="text-[11px] ml-1" htmlFor="password">Password</label>
           <div className="relative w-full">
             <input className="bg-custom-input-bg rounded-md w-full px-2 py-2 mt-1" type="text"  id="password" />
@@ -31,12 +31,12 @@ const Auth = () => {
           </div>
         </div>
 
-        <div className="w-full h-fit flex justify-between mt-3">
-          <div className="flex gap-2">
+        <div className="w-full h-fit flex flex-col justify-center items-center sm:flex-row sm:justify-between mt-3">
+          <div className="flex gap-2 w-[280px] sm:w-fit">
             <input type="checkbox" className=" text-[12px] appearance-none w-4 h-4 border border-custom-pink rounded-sm checked:bg-custom-pink checked:border-custom-pink focus:ring-0" id="check" />
             <label className="text-[11.5px]" htmlFor="check">Remember Me</label>
           </div>
-          <div className="text-[11.5px] underline" >Forgot Password?</div>
+          <div className="text-[11.5px] w-[280px] sm:w-fit underline" >Forgot Password?</div>
         </div>
 
         <button className="px-10 py-4 text-sm rounded-md bg-custom-pink text-black mt-8">Login</button>
